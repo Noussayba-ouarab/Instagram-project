949eac4766214cb97380ef02141dd68f57c24b66
NOUSSAYBA OUARAB <noussaibouarab@gmail.com>
2024-03-07 21:53:30 +0100

    Add new feature

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,12 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Support\Facades\DB;
-use App\Http\Requests\ProfileUpdateRequest;
 use App\Models\User;
-use  Illuminate\Database\Eloquent\ModelNotFoundException;
-
 use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -14,31 +9,29 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use Barryvdh\DomPDF\Facade\Pdf;
-use App\Http\Controllers\Storage;
 use App\Models\Poste;
 use App\Models\Like;
 use App\Models\commentaire;
-
+/**
+ * controlleur de l application
+ * 
+ */
 class ProfileController extends Controller
 {
+    /**
+     *
+     *
+     * @return View
+     */
     public function edit(): View
     {
-
         $user = Auth::user();
-        $userId = Auth::id();
+    
 
         $email = $user->email; // Access the email property
-        $username = $user->name;
+        
         $data = User::where('email', $email)->first();
-        //$posts = auth()->user()->postes()->get();
-        //$posts = Poste::where('username', $username)->first();
-        //$posts = Poste::where('user_id', $userId)->with('user')->get();
         $posts = Poste::with('user')->get();
-        // Save the new User instance to the database
-
-        // var_dump($posts->image);
-        // exit();
-        //$data = User::all();
 
         $numberposts = count($posts);
         return view("profile.partials.profile", [
@@ -46,10 +39,14 @@ class ProfileController extends Controller
             'posts' => $posts, 'numberposts' => $numberposts
         ]);
     }
+    /**
+     * download a file
+     *
+     * @return void
+     */
     public function download()
 
     {
-        //$data = User::all();
         $user = Auth::user();
         $email = $user->email; // Access the email property
         $data = User::where('email', $email)->first();
@@ -57,6 +54,12 @@ class ProfileController extends Controller
             ->setPaper('a4', 'portrait');
         return ($pdf->download('users-details.pdf'));
     }
+    /**
+     * 
+     *insert poste
+     * @param Request $request
+     * @return void
+     */
     public function add(Request $request)
     {
         $user = Auth::user();
@@ -77,17 +80,16 @@ class ProfileController extends Controller
             $data->save();
             $data = Poste::all();
             return redirect()->route('dashboard');
-            // return view('dashboard', array(
-            //     'data' =>  $data,
-            //     'postsWithcomments' => $postsWithcomments, 'likes' => $likes
-            // ));
         } else {
             return redirect()->route('dashboard');
-            // return view('dashboard', [
-            //     'postsWithcomments' => $postsWithcomments, 'likes' => $likes
-            // ]);
+            
         }
     }
+    /**
+     * page home
+     *
+     * @return View
+     */
     public function dashboard(): View
     {
         $user = Auth::user();
@@ -104,6 +106,11 @@ class ProfileController extends Controller
             return view("auth/login");
         }
     }
+    /**
+     * redirect to add poste page
+     *
+     * @return View
+     */
     public function adding(): View
     {
         $user = Auth::user();
@@ -113,6 +120,11 @@ class ProfileController extends Controller
             return redirect()->back();
         }
     }
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function profile()
     {
         $user = Auth::user();
@@ -122,6 +134,12 @@ class ProfileController extends Controller
             return redirect()->back();
         }
     }
+    /**
+     * insert comment
+     *
+     * @param Request $request
+     * @return void
+     */
     public function addcomment(Request $request)
     {
         $user = Auth::user();
@@ -139,14 +157,16 @@ class ProfileController extends Controller
             $comment = Commentaire::all();
             //return response()->json(['message' => 'Comment added successfully.']);
             return redirect()->back();
-            // return view('dashboard', [
-            //     'comment' => $comment,
-            //     'postsWithcomments'=>$postsWithcomments,'likes'=>$likes,
-            // ]);
         } else {
             return redirect()->back();
         }
     }
+    /**
+     * insert like or dislike to a post
+     *
+     * @param [type] $post_id
+     * @return void
+     */
     public function like($post_id)
     {
         // Check if the user already liked the post
@@ -155,77 +175,18 @@ class ProfileController extends Controller
             ->first();
         $postsWithcomments = Poste::with('commentaires')->get();
         $likes = Like::all();
-
-        // If the user already liked the post, return a response (you can handle this based on your application logic)
-        // if ($existingLike) {
-        //     return view('dashboard', ['postsWithcomments' => $postsWithcomments, 'likes' => $likes]);
-        // }
-
         // Create a new like
         $like = new Like();
         $like->user_id = auth()->id();
         $like->post_id = $post_id;
         $like->save();
         return response()->json(['success' => true, 'message' => 'Post liked successfully.']);
-        // You can also return a response if the like is successfully added
-        // return view('dashboard', ['postsWithcomments' => $postsWithcomments, 'likes' => $likes]);
+        
     }
-    public function unlike($postId)
-    {
-        $like = Like::where('user_id', auth()->id())->where('post_id', $postId)->first();
-
-        // If the like entry exists, delete it
-        if ($like) {
-            $like->delete();
-        }
-        // Optionally, you can update the number of likes for the post
-        $likesCount = Like::where('post_id', $postId)->count();
-        return redirect()->back();
-        // return response()->json(['success' => true, 'likes_count' => $likesCount]);
-    }
-
-    public function getOtherLikes($post_id)
-    {
-        //dd($post_id);
-        try {
-            $post = Poste::findOrFail($post_id);
-            dd($post);
-            return response()->json(['success' => true, 'message' => 'Post liked successfully.']);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Post not found'], 404);
-        }
-    }
-    public function registUser(Request $request): View
-    {
-        $data = new User();
-        $data->name = $request->name;
-        $data->email = $request->email;
-        $data->password = $request->password;
-        //trying to upload images
-        $image = $request->image;
-        if ($image) {
-            $imagename = time() . '.' . $image->getClientOriginalExtension();
-            $request->image->move('user', $imagename);
-            $data->image = $imagename;
-        }
-        $data->save();
-
-        return view('auth/login');
-    }
-    public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $request->user()->fill($request->validated());
-
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
-
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
-    }
+    
     /**
      * Delete the user's account.
+     * @param mixed $name
      */
     public function destroy(Request $request): RedirectResponse
     {
@@ -241,4 +202,3 @@ class ProfileController extends Controller
         return Redirect::to('/');
     }
 }
-?>
